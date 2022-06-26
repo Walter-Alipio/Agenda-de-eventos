03fff1cce4b1a09c40d335f38150482f328a8eca
Walter <silvawalteralipio@gmail.com>
2022-06-26 17:16:09 -0300

    rotas events

--- server/src/controllers/eventsController.ts
+++ server/src/controllers/eventsController.ts
@@ -4,27 +4,27 @@ import { Request, Response } from 'express';
 
 class EventController{
 
-  static listAll = (req: Request, res: Response) =>{
-      events.find((err , events)=>{
+  static listAll =  (req: Request, res: Response) =>{
+       events.find((err , events)=>{
       res.status(200).json(events)
    })
   }
 
-  static listEventById = (req: Request, res: Response) => {
-       const id = req.params.id;
-       events.findById(id,(err: mongoose.CallbackError, event: IEvent )=>{
+  static listEventById = async (req: Request, res: Response) => {
+       const id = await req.params.id;
+       await events.findById(id,(err: mongoose.CallbackError, event: IEvent )=>{
         err ? res.status(400).send({message: `${err.message} - Id não localizado`}) :
           res.status(200).send(event)
        })
   }
 
-  static newEvent = (req: Request, res: Response) =>{  
-      let {name, date, start, end, description} = req.body;
+  static newEvent = async (req: Request, res: Response) =>{  
+      let {name, date, start, end, description} = await req.body;
       date = new Date(date);
       //transformando em tipo Date para armazenar no bd
       let event = new events({name, date, start, end, description});
       
-      event.save((err: mongoose.CallbackError)=>{
+      await event.save((err: mongoose.CallbackError)=>{
 
         err ? res.status(500).send({message: `${err.message} - falha ao cadastrar evento`}) :
           res.status(200).send(event.toJSON())
@@ -32,20 +32,20 @@ class EventController{
       })
    }
 
-   static updateEvent = (req: Request, res: Response) => {
-      const id = req.params.id;
+   static updateEvent = async (req: Request, res: Response) => {
+      const id = await req.params.id;
 
-      events.findByIdAndUpdate(id,{$set: req.body}, (err: mongoose.CallbackError)=>{
+     await events.findByIdAndUpdate(id,{$set: req.body}, (err: mongoose.CallbackError)=>{
          !err ? 
             res.status(200).send({message: 'Atualizado com sucesso!'}) :
             res.status(500).send({messega: `${err.message} - Id não encontrado`})
       })
    }
 
-   static deleteEvent = (req: Request, res: Response) => {
-      const id = req.params.id;
+   static deleteEvent = async (req: Request, res: Response) => {
+      const id = await req.params.id;
 
-      events.findByIdAndDelete(id,(err: mongoose.CallbackError)=>{
+      await events.findByIdAndDelete(id,(err: mongoose.CallbackError)=>{
         !err ? 
           res.status(200).send({message: 'Evento excluído'}):
           res.status(500).send({message: `${err.message} - Id não encontrado`});
--- server/src/routes/eventsRoutes.ts
+++ server/src/routes/eventsRoutes.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import EventController from '../controllers/eventsController';
+import authMiddlewares from '../middlewares/auth'
 
-const router = express.Router();
+const eventRouter = express.Router();
 
-router
+eventRouter.use(authMiddlewares);//verifica se o usuário possuí um token autenticado
+
+eventRouter
   .get('/events',EventController.listAll)
   .get('/events/:id',EventController.listEventById)
   .post('/events',EventController.newEvent)
   .put('/events/:id', EventController.updateEvent)
   .delete('/events/:id', EventController.deleteEvent);
 
-export default router
+export default eventRouter;
