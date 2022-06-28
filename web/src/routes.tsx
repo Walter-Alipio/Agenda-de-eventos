1bc3452f7768a4253da27ba56c89b688b82eef10
Walter <silvawalteralipio@gmail.com>
2022-06-27 22:18:53 -0300

    add update event route

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import { CreateEvent } from "./pages/CreateEvent";
+import { Events } from "./pages/Events";
 import { Login } from "./pages/Login";
+import { NewUser } from "./pages/NewUser";
+import { UpdateEvent } from "./pages/UpdateEvent";
 
 
 export function Router(){
   return (
     <Routes>
       <Route path="/" element={<Login />}/>
-
+      <Route path="/newUser" element={<NewUser />}/>
+      <Route path="/home" element={<Events />}/>
+      <Route path="/newEvent" element={<CreateEvent />}/>
+      <Route path="/newEvent/:ID" element={<UpdateEvent />}/>
     </Routes>
   );
 }
