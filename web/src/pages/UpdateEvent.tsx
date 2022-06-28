5f24b68eab9981ff1743a7bdef4f9f425317b348
Walter <silvawalteralipio@gmail.com>
2022-06-28 15:29:42 -0300

    add function update

--- web/src/pages/UpdateEvent.tsx
+++ web/src/pages/UpdateEvent.tsx
@@ -3,51 +3,77 @@ import { BackButton } from "../components/BackButton";
 import { CreateEventForm } from "../components/forms/CreateEventForm";
 import { Header } from "../components/Header";
 import ModalOk from "../components/ModalOk";
+import { format } from "date-fns";
 
-import {useParams} from 'react-router-dom';
+import { useNavigate, useParams} from 'react-router-dom';
 import DataUser from "../auth/dataUser";
 import axios from 'axios';
 
 
 export function UpdateEvent(){
+  const navigate = useNavigate();
+
   const [name,setName] = useState('');
   const [date,setDate] = useState('');
   const [start,setStart] = useState('');
   const [end,setEnd] = useState('');
   const [description,setDescription] = useState('');
 
-  let [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
   let { ID } = useParams();
-   if(ID) {
-    // console.log(ID)
-    // axios({
-    // method: 'get',
-    // url:'http://localhost:3333/events/'+ID ,
-    // headers: {
-    //        Authorization: "Bearer " + DataUser.getToken()
-    //          }
-    //  })
-    // .then((res)=> console.log(res))
-    // .catch(err=>console.log(err))
-  }
 
-  function clear(){
-    setName('')
-    setDate('')
-    setStart('')
-    setEnd('')
-    setDescription('')
-  }
+  useEffect(()=>{
+   if(ID !== undefined) {
 
+    axios({
+    method: 'get',
+    url:'http://localhost:3333/events/'+ID ,
+    headers: {
+           Authorization: "Bearer " + DataUser.getToken()
+             }
+     })
+     .then((res)=> {
+      console.log(res.status)
+      const data = res.data;
+      
+      const [dateFormat] = data.date.toString().match(/(\d{4}-\d{2}-\d{2})/)
+      setName(data.name);
+      setDate(dateFormat);
+      setStart(data.start);
+      setEnd(data.end);
+      setDescription(data.description);   
+    })
+    .catch(err=>console.log(err))
+   }},[])
+  
   async function handleEvent(event: FormEvent){
   event.preventDefault();
 
-
-  
-      // clear();
-      // setIsOpen(true)
-    }
+    axios({
+      method: 'put',
+      url:'http://localhost:3333/events/'+ID ,
+      headers: {
+      Authorization: "Bearer " + DataUser.getToken()
+         },
+      data:{ 
+        name: name,
+        date: date,
+        start: start,
+        end: end,
+        description: description
+       } 
+    })
+    .then((res)=>{
+      console.log(res.status);
+      setIsOpen(true);
+      console.log(date)
+      // if(!isOpen){
+      //   navigate('/home');
+      // }
+    })
+    .catch((err)=>console.log(err))
+  }
 
   return (
     <>
