f13e623944cb4ecb833c12c3b1af5025c91e5631
Walter <silvawalteralipio@gmail.com>
2022-07-18 10:39:04 -0300

    adjust navigate

--- web/src/components/BackButton.tsx
+++ web/src/components/BackButton.tsx
@@ -1,14 +1,16 @@
 import { ArrowULeftUp, ArrowUUpLeft } from "phosphor-react";
+import { Link, useNavigate } from "react-router-dom";
 
-export function BackButton({href}:React.AnchorHTMLAttributes<HTMLAnchorElement>){
+export function BackButton(){
+  const navigate = useNavigate();
   return (
-    <a 
-      href={href}
+    <button
+      onClick={()=> navigate(-1)} 
       aria-label="Voltar" 
     >
       <ArrowUUpLeft size={24} 
         alt="Voltar" 
         className="w-11 h-11 bg-white absolute ml-3 mt-6 rounded-full flex items-center justify-center hover:shadow-md hover:shadow-sky-400 transition-shadow active:text-sky-400 p-3"/>
-    </a>
+    </button>
   )
 }
