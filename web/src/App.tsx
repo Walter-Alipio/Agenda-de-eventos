6555ae0780598c7221f3b127c2644a5d1b1a9471
Walter <silvawalteralipio@gmail.com>
2022-07-18 10:39:56 -0300

    add footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import {BrowserRouter} from 'react-router-dom'
+import Footer from './components/Footer'
 import { Router } from "./routes"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Router />
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+  return (
+    <footer className="w-screen h-16 bg-sky-800 bottom-0 text-sky-100 text-center flex flex-col items-center justify-center py-3">
+      <span> Desenvolvido por Walter Alípio </span>
+    </footer>
+  )
+}
